ad815b0fcf306eb40940571fca8d0714894b393b
Leandro Pitta <lspitta@gmail.com>
2023-07-08 13:18:27 -0300

    cabecalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuLateralComponent } from './home/menu-lateral/menu-lateral.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    MenuLateralComponent
   ],
   imports: [
     BrowserModule,
